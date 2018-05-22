d33eb311f9093810c43321e1e022e06200541698
nkostic97vr <nkostic97vr@gmail.com>
2018-05-22 23:55:06 +0200

    Dodaj sve kontrole u Login prozor

--- src/windows/Login.java
+++ src/windows/Login.java
@@ -7,12 +7,17 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
 
 public class Login extends JFrame {
 	private static final long serialVersionUID = 1L;
 	
 	private JPanel contentPane;
+	
 	private JTextField txtName;
+	private JTextField txtIp;
+	private JTextField txtPort;
 
 	/**
 	 * Create the frame.
@@ -29,14 +34,50 @@ public class Login extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+		// Name
+		JLabel lblName = new JLabel("Name:");
+		lblName.setBounds(64, 43, 165, 14);
+		contentPane.add(lblName);
+		
 		txtName = new JTextField();
 		txtName.setBounds(64, 61, 165, 20);
 		contentPane.add(txtName);
 		txtName.setColumns(10);
 		
-		JLabel lblName = new JLabel("Name:");
-		lblName.setBounds(64, 42, 46, 14);
-		contentPane.add(lblName);
+		// IP
+		JLabel lblIp = new JLabel("IP Address:");
+		lblIp.setBounds(64, 112, 165, 14);
+		contentPane.add(lblIp);
+		
+		txtIp = new JTextField();
+		txtIp.setColumns(10);
+		txtIp.setBounds(64, 131, 165, 20);
+		contentPane.add(txtIp);
+		
+		JLabel lblIpEg = new JLabel("(eg. 192.168.0.2)");
+		lblIpEg.setHorizontalAlignment(SwingConstants.CENTER);
+		lblIpEg.setBounds(64, 157, 165, 14);
+		contentPane.add(lblIpEg);
+		
+		// Port
+		JLabel lblPort = new JLabel("Port:");
+		lblPort.setBounds(64, 193, 165, 14);
+		contentPane.add(lblPort);
+		
+		txtPort = new JTextField();
+		txtPort.setColumns(10);
+		txtPort.setBounds(64, 210, 165, 20);
+		contentPane.add(txtPort);
+		
+		JLabel lblPortEg = new JLabel("(eg. 8192)");
+		lblPortEg.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPortEg.setBounds(64, 234, 165, 14);
+		contentPane.add(lblPortEg);
+		
+		// Login
+		JButton btnLogin = new JButton("Login");
+		btnLogin.setBounds(102, 289, 89, 23);
+		contentPane.add(btnLogin);
 	}
 	
 	/**
